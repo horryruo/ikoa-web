94145717fbec309770a18afb25fd57061f48140a
ruoxin <33121172+horryruo@users.noreply.github.com>
2020-10-20 23:22:23 +0800

    beta

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-if [[ -n $TEAM_DRIVE_ID && -n $RCLONE_DESTINATION && -n $LOG_PATH && -n $SA_JSON_1 && -n $SA_JSON_2 && -n $SERIAL_CODE && -n $MERGE_BOOL ]]; then
+if [[ -n $RCLONE_DESTINATION && -n $LOG_PATH && -n $ONEDRIVE_API_1 && -n $ONEDRIVE_API_2 && -n $SERIAL_CODE && -n $MERGE_BOOL ]]; then
     sed -i "/^serial/c\serial = \'$SERIAL_CODE\'" /app/fanza/config.toml   
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_1.conf
-    echo "team_drive = $TEAM_DRIVE_ID" >> /app/fanza/rclone_2.conf
-    echo "$SA_JSON_1" > /app/fanza/service_account_1.json
-    echo "$SA_JSON_2" > /app/fanza/service_account_2.json
+    echo "$ONEDRIVE_API_1" >> /app/fanza/rclone_1.conf
+    echo "$ONEDRIVE_API_2" >> /app/fanza/rclone_2.conf
+    
     if [[ $MERGE_BOOL == "false" ]]; then
         sed -i -e "/^merge/c\merge = false" -e "/^m3u_merge/c\m3u_merge = false" /app/fanza/config.toml
     fi
